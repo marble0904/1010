24400bb9d45c47b5c294f0e9a610f51831d20eed
marble0904 <40635513+marble0904@users.noreply.github.com>
2018-11-19 19:32:50 +0900

    プロジェクトを作成

---
+++ Assets/Scripts/NumberController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberController : MonoBehaviour {
+
+    public int[,] intValues = new int[4, 4]; 
+
+	// Use this for initialization
+	void Start () {
+		for(int i=0; i<4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
